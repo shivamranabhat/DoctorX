953c47b1a6ad8ad9985ded089295abf25ccc09cc
Shivam Ranabhat <shivamranabhat@gmail.com>
2025-05-31 22:57:09 +0100

    Added doctor page

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,19 +1,124 @@
-  $(".doctor-carousel").owlCarousel({
-        items: 3.5,
+
+  $(".hospital-card").owlCarousel({
+        items: 4,
         margin:30,
-        loop: true,
-        autoplay: true,
+        loop: false,
+        autoplay: false,
         autoplayTimeout: 5000,
         nav: false,
         dots: false,
         responsive: {
             // For large screens (desktop)
             1200: {
-                items: 3.7, // 3.5 items on large screens
+                items: 4, // 4 items on large screens
             },
             // For medium screens (tablets)
-            768: {
-                items: 2.5, // 3 items on medium screens
+            800: {
+                items: 2.5, // 2.5 items on medium screens
+            },
+            480: {
+                items: 1.2, // 1 and 20% items on medium screens
+            },
+            // For small screens (mobile)
+            0: {
+                items: 1.2, // 1.2 item on small screens
+            }
+        }
+    });
+  $(".radiology-card").owlCarousel({
+        items: 4,
+        margin:30,
+        loop: false,
+        autoplay: false,
+        autoplayTimeout: 5000,
+        nav: false,
+        dots: false,
+        responsive: {
+            // For large screens (desktop)
+            1200: {
+                items: 4, // 4 items on large screens
+            },
+            // For medium screens (tablets)
+            800: {
+                items: 2.5, // 2.5 items on medium screens
+            },
+            480: {
+                items: 1.2, // 1 and 20% items on medium screens
+            },
+            // For small screens (mobile)
+            0: {
+                items: 1.2, // 1.2 item on small screens
+            }
+        }
+    });
+  $(".diagnostic-card").owlCarousel({
+        items: 4,
+        margin:30,
+        loop: false,
+        autoplay: false,
+        autoplayTimeout: 5000,
+        nav: false,
+        dots: false,
+        responsive: {
+            // For large screens (desktop)
+            1200: {
+                items: 4, // 4 items on large screens
+            },
+            // For medium screens (tablets)
+            800: {
+                items: 2.5, // 2.5 items on medium screens
+            },
+            480: {
+                items: 1.2, // 1 and 20% items on medium screens
+            },
+            // For small screens (mobile)
+            0: {
+                items: 1.2, // 1.2 item on small screens
+            }
+        }
+    });
+  $(".doctor-card").owlCarousel({
+        items: 4,
+        margin:30,
+        loop: false,
+        autoplay: false,
+        autoplayTimeout: 5000,
+        nav: false,
+        dots: false,
+        responsive: {
+            // For large screens (desktop)
+            1200: {
+                items: 4, // 4 items on large screens
+            },
+            // For medium screens (tablets)
+            800: {
+                items: 2.5, // 2.5 items on medium screens
+            },
+            480: {
+                items: 1.2, // 1 and 20% items on medium screens
+            },
+            // For small screens (mobile)
+            0: {
+                items: 1, // 1.2 item on small screens
+            }
+        }
+    });
+  $(".know-more").owlCarousel({
+        items: 3,
+        margin:30,
+        loop: false,
+        autoplay: false,
+        autoplayTimeout: 5000,
+        nav: false,
+        dots: false,
+        responsive: {
+            // For large screens (desktop)
+            1200: {
+                items: 3, // 3 items on large screens
+            },
+            // For medium screens (tablets)
+            767: {
+                items: 2.5, // 2.5 items on medium screens
             },
             480: {
                 items: 1.2, // 1 and 20% items on medium screens
